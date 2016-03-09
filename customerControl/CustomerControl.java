203478f44cca407367473a6475aaf1a7ee7ad8f5
gudnims <gudnims@gmail.com>
2016-03-09 08:59:15 +0100

    customer

---
+++ customerControl/CustomerControl.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package customerControl;
+
+/**
+ *
+ * @author Gudni
+ */
+public class CustomerControl {
+    
+    bank Bank();
+
+    public CustomerControl() {
+        
+        public String login(String username, String password){
+            
+            return customer;
+            
+        }
+        
+        public boolean checkAcount(){
+            
+            return account;
+        }
+        
+        public 
+    }
+    
+    
+    
+}
